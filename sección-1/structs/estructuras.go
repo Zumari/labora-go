35e92ca98e6d83e4f3b9fede4bef4c22616520ce
Zumari <fernanda_zunigaa@yahoo.com>
2023-04-09 21:34:13 -0600

    ejercicio corregido

--- sección-1/structs/estructuras.go
+++ sección-1/structs/estructuras.go
@@ -4,28 +4,37 @@ import (
 	"fmt"
 )
 
-func main() {
-
-	maria := Persona{nombre: "Maria", edad: 25, ciudad: "Tegucigalpa", telefono: 99999999}
-	carlos := Persona{nombre: "Carlos", edad: 40, ciudad: "Bogota", telefono: 8888855555}
-
-	fmt.Printf("Persona 1: %v\nPersona 2: %v\n", maria, carlos)
-
-	cambiarCiudad(&maria)
-
-	fmt.Printf("Persona 1 con la ciudad actualizada: %v\nPersona 2: %v\n", maria, carlos)
-
-}
-
-func cambiarCiudad(persona *Persona) {
-
-	persona.ciudad = "Valencia"
-
-}
-
 type Persona struct {
 	nombre   string
 	edad     int
 	ciudad   string
 	telefono int
+}
+
+// Implementación del constructor NewPersona
+func newPersona(nombre string, edad int, ciudad string, telefono int) Persona {
+	return Persona{nombre: nombre, edad: edad, ciudad: ciudad, telefono: telefono}
+}
+
+// Implementación del método cambiarCiudad
+func (p *Persona) cambiarCiudad(nuevaCiudad string) {
+	p.ciudad = nuevaCiudad
+}
+
+// Implementación de la función String para la estructura Persona
+func (p Persona) String() string {
+	return fmt.Sprintf("{nombre: %s, edad: %d, ciudad: %s, telefono: %d}", p.nombre, p.edad, p.ciudad, p.telefono)
+}
+
+func main() {
+
+	maria := newPersona("Maria", 25, "Tegucigalpa", 99999999)
+	carlos := newPersona("Carlos", 40, "Bogota", 8888855555)
+
+	fmt.Printf("Persona 1: %v\nPersona 2: %v\n", maria, carlos)
+
+	maria.cambiarCiudad("Cartagena")
+
+	fmt.Printf("Persona 1 con la ciudad actualizada: %v\nPersona 2: %v\n", maria, carlos)
+
 }
