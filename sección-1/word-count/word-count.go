3ff7d0f24c6bddb82ff5ae109123dcc8930a6f3a
Zumari <fernanda_zunigaa@yahoo.com>
2023-04-11 20:35:37 -0600

    word-count

---
+++ sección-1/word-count/word-count.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func WordCount(texto string) map[string]int {
+	contador := strings.Fields(texto)
+	contadorPalabras := make(map[string]int)
+
+	for _, palabras := range contador {
+
+		contador, ok := contadorPalabras[palabras]
+		if ok {
+			contador = contador + 1
+		}
+		if !ok {
+			contador = 1
+		}
+		contadorPalabras[palabras] = contador
+	}
+	fmt.Println(contadorPalabras)
+	return contadorPalabras
+}
+
+func main() {
+
+	texto := "I ate a donut. Then I ate another donut. "
+	WordCount(texto)
+}
