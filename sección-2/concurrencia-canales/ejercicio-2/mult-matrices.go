34b51b0e24307b273c807595d6782c6fd273934e
Zumari <fernanda_zunigaa@yahoo.com>
2023-04-25 22:27:45 -0600

    ejercicio api

---
+++ sección-2/concurrencia-canales/ejercicio-2/mult-matrices.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+
+func multiplicarMatrices(matrizA [][]int, matrizB [][]int) {
+
+}
+
+func main() {
+	var matrizA [][]int = [][]int{{1, 2, 3}, {4, 5, 6}}
+	var matrizB [][]int = [][]int{{1, 2}, {2, 3}, {3, 4}}
+
+	multiplicarMatrices(matrizA, matrizB)
+	fmt.Println("Hi")
+}
